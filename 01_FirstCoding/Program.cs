61403449eed78bcd7242563848cd1bdc64514586
Martin <martin_lenart@icloud.com>
2024-08-30 11:17:48 +0200

    initial commit

---
+++ 01_FirstCoding/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Program Begin!");
+
+int sum = 0;
+for (int i = 0; i < 4; i++)
+{
+    sum = sum + i;
+    System.Console.WriteLine($"i = {i}");
+    System.Console.WriteLine($"sum = {sum}");
+
+    if (sum == 3)
+    {
+        System.Console.WriteLine($"We have now the magical sum of {sum}");
+    }
+    System.Console.WriteLine();
+}
+
+Console.WriteLine("Program End!");
---
+++ 01_FirstCoding/readme.txt
@@ -0,0 +1,25 @@
+Skapa ett ny Console Applikation
+
+Uppgift1
+Skriv ett program som ger nedan utskrift:
+
+Program begin
+
+i = 0
+sum = 0
+
+i = 1
+sum = 1
+
+i = 2
+sum = 3
+We have now the magical sum of 3
+
+i = 3
+sum = 6
+
+Program end
+
+Gör:
+Git commit
+Publicera på GitHub
