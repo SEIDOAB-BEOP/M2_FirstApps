15377ec3a931256269121ae75a724300883fdb71
Martin <martin_lenart@icloud.com>
2024-09-02 15:36:11 +0200

    update

--- 05_struct_array/Program.cs
+++ 05_struct_array/Program.cs
@@ -1,15 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
 
 stBook[] books = new stBook[]{
     new stBook(){ Title = "Lord of the rings", Author = "J.R.R Tolkien", YearPublished = 1959}, 
-    new stBook(){ Title = "Harry Potter", Author = "J.K Rowland", YearPublished = 2010}
+    new stBook(){ Title = "Harry Potter", Author = "J.K Rowland", YearPublished = 2010},
+    new stBook(){ Title = "Alfons", Author = "E. Stuart", YearPublished = 2020}
     };
+
+
+for (int i = 0; i < books.Length; i++)
+{
+    System.Console.WriteLine(books[i]);
+}
 
 
 public struct stBook {
     public string Title;
     public string Author;
     public int YearPublished;
+
+    public override string ToString() => $"The book {Title}, written by {Author} was published year {YearPublished}."; 
 }
---
+++ 06_static/Program.cs
@@ -0,0 +1,38 @@
+﻿stAnimal animal1 = new stAnimal(){ Name = "Wanda", Kind = "Fish", Age = 4};
+System.Console.WriteLine(animal1);
+stAnimal animal2 = new stAnimal(){ Name = "Simba", Kind = "Lion", Age = 5};
+System.Console.WriteLine(animal2);
+
+
+
+stAnimal[] animals = new stAnimal[]{
+    animal1, animal2,
+    new stAnimal(){ Name = "Max", Kind = "Dog", Age = 2},
+    new stAnimal(){ Name = "Woffie", Kind = "Cat", Age = 5},
+    new stAnimal(){ Name = "Dumbo", Kind = "Elephant", Age = 15}
+    };
+
+stAnimal.Zoo = "Kalmaarden";
+int sum = 0;
+for (int i = 0; i < animals.Length; i++)
+{
+    System.Console.WriteLine(animals[i]);
+
+    //System.Console.WriteLine($"Hi from {animals[i].Name}. I'm {animals[i].Age} years old!");
+    System.Console.WriteLine(stAnimal.Zoo);
+    sum = sum + animals[i].Age;
+}
+System.Console.WriteLine($"Total age: {sum}");
+
+
+public struct stAnimal {
+    public string Name;
+    public string Kind;
+    public int Age;
+
+    public static string Zoo = "Skansen";
+
+    public override string ToString() {
+        return $"Hello from {Name}. I'm a {Age} year old {Kind}!";
+    }
+}
