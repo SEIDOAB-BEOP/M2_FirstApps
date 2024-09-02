3721acff377d314ca0b85570d9980c97bcb3116f
Martin <martin_lenart@icloud.com>
2024-09-02 09:56:47 +0200

    new project

---
+++ 02_for_if/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int i = 0;
+for (;;)
+{
+    i++;
+    System.Console.WriteLine(i);
+
+    if (i == 5)
+    {
+        break;
+    }
+
+}
