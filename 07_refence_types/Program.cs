68fae7ceb5d698650a81f3cea2fc05aaf29c7997
Martin <martin_lenart@icloud.com>
2024-09-03 11:06:37 +0200

    update

---
+++ 07_refence_types/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("VALUE TYPE!");
+stPerson p1 = new stPerson(){FirstName="Severus",LastName="Snape"};
+stPerson p2 = p1;
+p2.FirstName = "Sam";
+p2.LastName = "Gamgee";
+
+System.Console.WriteLine(p1);
+System.Console.WriteLine(p2);
+
+
+Console.WriteLine("Class is REFERENCE TYPE!");
+csPerson p3 = new csPerson(){FirstName="Severus",LastName="Snape"};
+csPerson p4 = p3;
+p4.FirstName = "Sam";
+p4.LastName = "Gamgee";
+
+System.Console.WriteLine(p3);
+System.Console.WriteLine(p4);
+
+
+Console.WriteLine("Array is REFERENCE TYPE!");
+
+int[] array1 = new int[]{1,2,3,4,5,6};
+int[] array2 = array1;
+
+array2[0] = 100;
+System.Console.WriteLine(array1[0]);
+System.Console.WriteLine(array2[0]);
+
+
+public struct stPerson
+{
+    public string FirstName;
+    public string LastName;
+
+    public override string ToString() => $"Hi, I'm {FirstName} {LastName}";
+}
+
+public class csPerson
+{
+    public string FirstName;
+    public string LastName;
+    public override string ToString() => $"Hi, I'm {FirstName} {LastName}";
+}
---
+++ 08_static_apple/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/*
+1. Model an apple in a class, csApple.
+   An Apple has a weight in gram (e.g. 83) and a kind (e.g. "Granny Smith")
+2. Make a ToString() => $"" that presents an apple
+3. Create 5 instances of various apples and print then out
+4. Modify csApple to have a static field nrOfApples and a constructor that automatically increments nrOfApples
+5. Print out Nr of apples instanstiated.
+*/
