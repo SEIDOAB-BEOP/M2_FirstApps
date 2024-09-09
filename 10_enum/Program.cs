263ab79ffad80994fd06e22acb7394276412d5d0
Martin <martin_lenart@icloud.com>
2024-09-09 10:59:56 +0200

    update

---
+++ 10_enum/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var a1 = new stAnimal() {Name = "Max", Age = 5, Kind = enAnimalKind.dog, Mood= enAnimalMood.hungry};
+var a2 = new stAnimal() {Name = "Wanda", Age = 2, Kind = enAnimalKind.fish, Mood= enAnimalMood.sleeeeepy};
+
+System.Console.WriteLine(a1);
+System.Console.WriteLine(a2);
+
+public enum enAnimalKind {dog, cat, bird, fish}
+public enum enAnimalMood {sleepy, happy, hungry}
+
+public struct stAnimal
+{
+    public enAnimalKind Kind;
+    public enAnimalMood Mood;
+    public string Name;
+    public int Age;
+
+    public override string ToString() => $"{Name} the {(int)Mood} {(int)Kind} is {Age} years old.";
+}
+
+
+
+//Excerices:
+//1.    Declare a public enum type PlayingCardColor that models Spade, Club, Heart, Diamond 
+//2.    Declare a public enum type PlayingCardValue that models Ace, Two..Ten, Knight, Queen, King.
+///     Ace should start with constant = 1
+//3.    Declare a public struct PlayingCard that models Color and Value of a playing card. 
+//4.    Write a ToString() that presents tha card, e.g Queen of Heart, two of Clubs
+//5.    Create 3 playing cards and print them out using Console.WriteLine
---
+++ 14_enum_string/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+    var s1 = new string[] {"A Tale of Two Cities", "Charles Dickens", "English", "1859", "200 million"};
+    var s2 = new string[] {"Harry Potter and the Philosopher's Stone", "J. K. Rowling", "English", "1997", "120 million"};
+    var s3 = new string[] {"And Then There Were None", "Agatha Christie", "English", "1939", "100 million"};
+    var s4 = new string[] {"Dream of the Red Chamber", "Cao Xueqin", "Chinese", "1791", "100 million"};
+    var s5 = new string[] {"The Hobbit", "JRR. Tolkien", "English", "1937", "100 million"};
+    var s6 = new string[] {"The Little Prince", "Antoine de Saint-Exupéry", "French", "1943", "150 million"};
+
+
+/*
+1. Create an enum type that can contain the values English, Chinese, French
+
+*/
