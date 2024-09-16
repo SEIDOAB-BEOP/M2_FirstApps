649afb4e302fde76e2cb1e5c2112b53da7e66dc4
Martin <martin_lenart@icloud.com>
2024-09-16 14:58:20 +0200

    update

---
+++ 16_methods/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+long area, circumference;
+double diagonal;
+
+GetACD(1,1, out area,out circumference, out diagonal);
+System.Console.WriteLine($"area: {area} circumference: {circumference} diagonal: {diagonal}");
+
+
+GetACD(2,2, out area,out circumference, out diagonal);
+System.Console.WriteLine($"area: {area} circumference: {circumference} diagonal: {diagonal}");
+
+
+(long, long, double) res = GetACD1(2, 2);
+System.Console.WriteLine($" area: {res.Item1} circumference: {res.Item2} diagonal: {res.Item3}");
+
+(long area, long circumference, double diagonal) res1 = GetACD1(2, 2);
+System.Console.WriteLine($" area: {res1.area} circumference: {res1.circumference} diagonal: {res1.diagonal}");
+
+
+static void GetACD(long width, long height,
+    out long area, out long circumference, out double diagonal)
+{
+    area = width * height;
+    circumference = 2 * width + 2 * height;
+    diagonal = Math.Sqrt(width * width + height + height);
+}
+
+static (long, long, double) GetACD1(long width, long height)
+{
+    var area = width * height;
+    var circumference = 2 * width + 2 * height;
+    var diagonal = Math.Sqrt(width * width + height + height);
+
+    return (area, circumference, diagonal);
+}
---
+++ 16_methods_value_ref/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var a1 = 2L;
+var a2 = 2L;
+
+var area1 = AreaVal(a1, a2);
+System.Console.WriteLine($"Area {area1}, a2 = {a2}");
+
+var rect = new Rect(){width = 2, height= 2};
+var area2 = AreaRef(rect);
+System.Console.WriteLine($"Area {area2}, width: {rect.width}");
+
+static long AreaRef(Rect rectparam)
+{
+    var area = rectparam.width * rectparam.height;
+    rectparam.width = 100;
+    return area;
+}
+
+static long AreaVal(long width, long height)
+{
+
+    var area = width * height;
+
+    width = 100;
+    return area;
+}
+
+class Rect 
+{
+    public long width;
+    public long height;
+}
+
